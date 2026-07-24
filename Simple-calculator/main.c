c90f468dd67d2f04ed9e5e49fa3b71c0d8434370
Wachirawit-Kunjai <tonkla001234@gmail.com>
2026-07-24 22:37:27 +0700

    Implement basic calculator

--- Simple-calculator/main.c
+++ Simple-calculator/main.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+int main()
+{
+    float sum;
+    int n1, n2, op;
+
+    while (1)
+    {
+        printf("Enter the first number\n");
+        scanf("%d", &n1);
+        printf("Enter the operator\n");
+        scanf("%d", &op);
+        printf("Enter the second number\n");
+        scanf("%d", &n2);
+        switch (op)
+        {
+        case 1:
+            sum = n1 + n2;
+            printf("%d + %d = %.2f\n", n1, n2, sum);
+            break;
+        case 2:
+            sum = n1 - n2;
+            printf("%d - %d = %.2f\n", n1, n2, sum);
+            break;
+        case 3:
+            sum = n1 * n2;
+            printf("%d × %d = %.2f\n", n1, n2, sum);
+            break;
+        case 4:
+            sum = n1 / n2;
+            printf("%d ÷ %d = %.2f\n", n1, n2, sum);
+            break;
+        default:
+            printf("Invalid");
+            break;
+        }
+    }
+}
